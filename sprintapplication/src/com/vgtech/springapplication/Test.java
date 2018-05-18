7ba9eff157f2cf25c77dcec2f2c9cdf82c929a08
Vikram Ilangovan <39159449+vikramilangovan@users.noreply.github.com>
2018-05-18 11:59:10 +0530

    springapplication

---
+++ sprintapplication/src/com/vgtech/springapplication/Test.java
@@ -0,0 +1,17 @@
+package com.vgtech.springapplication;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+
+  
+
+public class Test {
+	public static void main(String args[]) {
+		Resource resource=new ClassPathResource("applicationContext.xml");
+		BeanFactory factory=new XmlBeanFactory(resource);
+		Student student=(Student) factory.getBean("studentbean");
+		student.displayInfo();
+	}
+}
