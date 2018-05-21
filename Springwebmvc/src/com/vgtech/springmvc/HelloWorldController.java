421bde8e8dad1ed3b9d6a39f85a0ff9236421f3c
Vikram Ilangovan <39159449+vikramilangovan@users.noreply.github.com>
2018-05-21 14:20:44 +0530

    spring mvc

---
+++ Springwebmvc/src/com/vgtech/springmvc/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.vgtech.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller  
+public class HelloWorldController {
+	@RequestMapping("hello.html")  
+	public ModelAndView helloWorld(){
+		String message = "Hello spring mvc";
+		return new ModelAndView("hellopage", "message", message);
+	}
+}
+
+
